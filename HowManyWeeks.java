05432229870b4af73ce73a069a173c95c009c9ff
Vaishali Budhiraja <budhirajavaishali@gmail.com>
2017-10-02 00:34:19 +0530

    Update HowManyWeeks.java

--- HowManyWeeks.java
+++ HowManyWeeks.java
@@ -1,4 +1,18 @@
+import java.util.Scanner; //importing scanner class 
 
-public class HowManyWeeks {
+public class HowManyWeeks { // creating the class
+	public static void main(String[] args) { // starting the main method
+		// initializing variables
+		int n, res;
+		// Input
+		System.out.println("Please enter the weekly water usage (in gallons): ");
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		// Business logic (Processing)
+		res = 10000 / n;
+		// Output
+		System.out.println("The water will be sufficient for " + res + " weeks");
+		sc.close(); // closing scanner object
+	}
 
 }
